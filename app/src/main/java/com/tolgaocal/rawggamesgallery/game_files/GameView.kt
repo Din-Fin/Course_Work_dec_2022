d13959c4bd7dadd3ed5b61ae9e62d762d2bad402
Din-Fin <76566901+Din-Fin@users.noreply.github.com>
2022-06-11 00:01:37 +0700

    Initial commit

---
+++ app/src/main/java/com/tolgaocal/rawggamesgallery/game_files/GameView.kt
@@ -0,0 +1,57 @@
+package com.tolgaocal.rawggamesgallery.game_files
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.tolgaocal.rawggamesgallery.Commons.fetchImage
+import com.tolgaocal.rawggamesgallery.R
+import com.tolgaocal.rawggamesgallery.SingletonGame
+import kotlinx.android.synthetic.main.game.*
+
+
+// details screen
+class GameView : AppCompatActivity() {
+
+    private val viewModel by lazy { GameViewModel(GameModel(this)) }
+
+    // take the chosen game_files item
+    val gameItem = SingletonGame.currentGameItem!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game)
+        title = "Детали"
+
+        // initialize attributes to views
+        nameText.text = gameItem.name
+        descriptionText.text = gameItem.description
+        fetchImage(gameItem.image, gameImage, this)
+        releaseDateText.text = gameItem.releaseDate
+        metacriticText.text = gameItem.metacritic
+
+        // set favorite icon
+        favoriteImage.apply {
+            favoriteImage.setImageResource(if (gameItem.favorite) R.drawable.ic_favorite_heart else R.drawable.ic_empty_heart)
+            this.setOnClickListener {
+                changeFavoriteStatus(it)
+            }
+        }
+
+    }
+
+
+    // change favorite status
+    private fun changeFavoriteStatus(it : View) {
+        gameItem.favorite = !gameItem.favorite
+        favoriteImage.setImageResource(if (gameItem.favorite) R.drawable.ic_favorite_heart else R.drawable.ic_empty_heart)
+        viewModel.updateFavorite(gameItem)
+        Snackbar.make(it,if (gameItem.favorite) "Добавлено в избранное!" else "Удалено из избранного!",Snackbar.LENGTH_SHORT)
+            .setBackgroundTint(if (gameItem.favorite) Color.rgb(5,70,5) else Color.BLACK).show()
+    }
+
+
+}
